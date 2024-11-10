d98cf7fa5333f43224dbf5d8f08f955287ae53d9
Mahendra <pawdemahendra13@gmail.com>
2024-11-10 23:37:18 +0530

    This is my first commit

--- src/GitDemo/Demo.java
+++ src/GitDemo/Demo.java
@@ -10,3 +10,11 @@ System.out.println("This is My Git Demo Prject");
 }
 
 //https://github.com/Mahendra-Pawde347/GitDemoProject2.git
+
+//echo "# GitDemoProject2" >> README.md
+//git init
+//git add README.md
+//git commit -m "first commit"
+//git branch -M main
+//git remote add origin https://github.com/Mahendra-Pawde347/GitDemoProject2.git
+//git push -u origin main
