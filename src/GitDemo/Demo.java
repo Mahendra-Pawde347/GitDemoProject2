d96a85c9ce95a4c2012052e87a1db5b2e120a74e
Mahendra <pawdemahendra13@gmail.com>
2024-11-10 22:31:16 +0530

    This is my First commit

---
+++ src/GitDemo/Demo.java
@@ -0,0 +1,12 @@
+package GitDemo;
+
+public class Demo 
+{
+public static void main(String[] args) 
+{
+System.out.println("This is My Git Demo Prject");
+	
+}
+}
+
+//https://github.com/Mahendra-Pawde347/GitDemoProject2.git
